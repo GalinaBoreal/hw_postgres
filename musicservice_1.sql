4e2526193496a872d6bc57b49c6dfecffdd0be64
GalinaBoreal <galina_ver@mail.ru>
2023-06-02 10:31:06 +0500

    created collectionrecording

--- musicservice_1.sql
+++ musicservice_1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Genre (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(20) NOT NULL
+	name VARCHAR(20) NOT null UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Musician (
@@ -19,7 +19,7 @@ create table if not exists Album (
 	id SERIAL primary key,
 	name VARCHAR(20) not null,
 	relise DATE not null,
-	description TEXT 
+	description TEXT
 );
 
 create table if not exists AlbumRecording (
@@ -32,7 +32,7 @@ create table if not exists AlbumRecording (
 create table if not exists Track (
 	id SERIAL primary key,
 	name VARCHAR(20) not null,
-	t_time TIME not null,
+	t_time INTEGER not null CONSTRAINT time_track CHECK (t_time > 0),
 	album_id INTEGER REFERENCES Album(id) not null
 );
 
@@ -40,7 +40,14 @@ create table if not exists Collection (
 	id SERIAL primary key,
 	name varchar(40) not null,
 	relise DATE not null,
-	description text,
-	track_id INTEGER REFERENCES Track(id) not null
+	description text
 );
+
+CREATE TABLE IF NOT EXISTS CollectionRecording (
+	id SERIAL primary key,
+	track_id INTEGER REFERENCES Track(id) not null,
+	collection_id INTEGER REFERENCES Collection(id) not null,
+	description TEXT
+);
+
 commit
